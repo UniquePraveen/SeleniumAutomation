9cecf4cff4756cf2645225d919b63d1225171459
UniquePraveen <htnpraveen@gmail.com>
2025-07-21 22:51:37 +0530

    UploadFunctionality

--- src/test/java/UploadDownloadFunctions.java
+++ src/test/java/UploadDownloadFunctions.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -20,28 +21,51 @@ public class UploadDownloadFunctions {
     }
 
 
+//    @Test
+//    public void downloadFile() {
+//
+//        driver.findElement(By.id("j_idt93:j_idt95")).click();
+//
+//        File file = new File("/Users/praveen/Downloads");
+//        File[] totalFiles = file.listFiles();
+//
+//        Assert.assertNotNull(totalFiles);
+//        for (File findFile : totalFiles) {
+//
+//
+//            if (findFile.getName().equals("TestLeaf Logo.png ")) {
+//                System.out.println("File Found");
+//                break;
+//
+//            }
+//
+//        }
+//
+//
+//    }
+
     @Test
-    public void downloadFile() {
+    public void uploadFile() {
 
-        driver.findElement(By.id("j_idt93:j_idt95")).click();
+        String filePath = "/Users/praveen/Downloads/ITS4202_Emerging_Technologies_assignII .pdf";
 
-        File file = new File("/Users/praveen/Downloads");
-        File[] totalFiles = file.listFiles();
-
-        Assert.assertNotNull(totalFiles);
-        for (File findFile : totalFiles) {
+        // Step 1: Locate the input element
+        // You can send file path directly using sendKeys
+        driver.findElement(By.xpath("//input[@id=\"j_idt88:j_idt89_input\"]")).sendKeys(filePath);
 
 
-            if (findFile.getName().equals("TestLeaf Logo.png ")) {
-                System.out.println("File Found");
-                break;
 
-            }
 
+
+
+
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
         }
-
-        driver.quit();
-
-
     }
 }
